f027da7d69d7ce04741d449205529972cda5d7ef
sharmasatyam121104-devloper <sharma.satyam121104@gmail.com>
2026-01-24 10:13:19 +0530

    feat: create user mongoose schema

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+interface IUser {
+    name: string;
+    email: string;
+    password: string;
+    mobile?: string;
+    role: "user" | "deliveryBoy" | "admin"
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    mobile: {
+        type: String,
+        trim: true,
+        sparse: true
+    },
+    role: {
+        type: String,
+        enum: ["user", "deliveryBoy", "admin"],
+        default: "user",
+    }
+},{timestamps:true})
+
+const UserModel = mongoose.models.User || mongoose.model("User", userSchema)
+
+export default UserModel
