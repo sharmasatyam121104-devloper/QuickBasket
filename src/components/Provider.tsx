24a935f32c1c563f436c080c866f23505f440e65
sharmasatyam121104-devloper <sharma.satyam121104@gmail.com>
2026-01-26 00:25:52 +0530

    fix: prevent Ant Design style break on page reload in Next.js

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,12 +1,15 @@
 'use client'
 import { SessionProvider } from 'next-auth/react'
 import React from 'react'
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 const Provider = ({children}:{children:React.ReactNode}) => {
   return (
-   <SessionProvider>
-        {children}
-   </SessionProvider>
+    <AntdRegistry>
+        <SessionProvider>
+                {children}
+        </SessionProvider>
+    </AntdRegistry>
   )
 }
 
